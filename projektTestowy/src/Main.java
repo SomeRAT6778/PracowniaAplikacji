7fb38e92d825fd12ea64d94f4cbe94cff5b7c8c0
rat <rat@email>
2026-09-08 12:30:51 +0200

    komentarz

--- projektTestowy/src/Main.java
+++ projektTestowy/src/Main.java
@@ -3,11 +3,10 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    System.out.println("hello world");
+    int calkowita = 5;
+    double rzeczywista = 1.5;
+    System.out.println("wartosc zmiennej \"calkowita\" wynosi + calkowita");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+
 }

eb43abfbcc80da58cca57ca91cbacec720f60e35
mikolaj klimek <adres@email>
2026-09-11 18:08:15 +0900

    zadanie 3

--- zadanie1/src/Main.java
+++ zadanie1/src/Main.java
@@ -1,5 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    //zadanie1
+    System.out.println("Ania");
+    System.out.println("Bartek");
+    System.out.println("Kasia");
+
+    //zadanie2
+    String imie ="Mikołaj";
+    int rok_urodzenia=2007;
+    double liczba =0.66;
+
+    //zadanie3
+    int rok=2026;
+    System.out.println("Mam na imie "+imie+" mam "+(rok - rok_urodzenia)+
+            " lat i bede pisac mature za "+liczba+" roku.");
+    //zadanie4
     
+
 }

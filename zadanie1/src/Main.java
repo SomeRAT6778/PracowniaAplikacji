109dff93d23ccd839df552de2b3d4f01a2129a93
mikolaj klimek <adres@email>
2026-09-11 18:22:30 +0900

    zadania 4

--- zadanie1/src/Main.java
+++ zadanie1/src/Main.java
@@ -16,6 +16,11 @@ void main() {
     System.out.println("Mam na imie "+imie+" mam "+(rok - rok_urodzenia)+
             " lat i bede pisac mature za "+liczba+" roku.");
     //zadanie4
-    
+    System.out.println("podaj ile jest stopni na dworze w stopniach ceciusza");
+    Scanner sc =new Scanner(System.in);
+    double stopnie = sc.nextDouble();
+    double fahraheit = 1.8*stopnie+32.0;
+    System.out.println("na dworze jest "+fahraheit+"stopni Fahrenheita");
+
 
 }
